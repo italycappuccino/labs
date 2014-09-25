4ea0116c4605a6eea6830db3a4eb9a48ec5431b0
Peter Liu <italycappuccino@gmail.com>
2014-09-25 16:07:18 +0800

    test
    test

---
+++ src/main/java/com/stone/core/service/MessageService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.stone.core.service;
+
+/**
+ * @author Peter
+ * @create 2014年8月30日
+ * @TODO
+ */
+public interface MessageService {
+
+    String getMessage();
+
+}
---
+++ src/main/java/com/stone/core/service/impl/MessageServiceImpl.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package com.stone.core.service.impl;
+
+/**
+ * @author Peter
+ * @create 2014年8月30日
+ * @TODO
+ */
+public class MessageServiceImpl {
+
+}
